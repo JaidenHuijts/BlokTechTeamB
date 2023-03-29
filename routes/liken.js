c8e8dbe94d8b893e9c05ed1d6a76b17ef570c2f6
kyara.frankena@hva.nl <kyara.frankena@hva.nl>
2023-03-29 14:04:15 +0200

    nieuwe versie

--- routes/liken.js
+++ routes/liken.js
@@ -6,3 +6,42 @@ router.get('/', (req, res) => {
 })
 
 module.exports =  router
+
+//NODIG OM INFO UIT DATABASE TE HALEN!
+// app.get('/liken', async (req, res) => {
+//     // logic db call -> template aanroepen -> de data meegeven aan de template ||
+//     const collection = client.db ('MaeveInterior').collection('interior');
+//     const interiorList = await collection.find ({}).toArray();
+
+//     res.render("liken.ejs",  {interiorList});
+// })
+
+
+
+//OM TE KUNNEN ZIEN OF DIE LIKE TRUE OF FALSE IS
+// app.post('/likepagina',  async (req, res) => {  
+//     // console.log('@@-- REQ. body', req.body);
+
+//     console.log(JSON.parse(JSON.stringify(req.body)))
+
+//     try {
+//         // definieër collection en filter
+//         const collection = client.db ('MaeveInterior').collection('interior');
+//         const filter = {_id: new ObjectId(req.body.itemid)}
+
+//         // zoek inspiratie beeld uit db
+//         const inspiratieBeeld = await collection.findOne(filter);
+        
+//         // wanneer inspiratie beeld geliked unlike anders like
+//         if (inspiratieBeeld.liked) {
+//             const result = await collection.updateOne(filter, {$set: {liked: false}});
+//         } else {
+//             const result = await collection.updateOne(filter, {$set: {liked: true}});
+//         }
+//         // redirect naar overzicht pagina
+//         res.redirect('/overzicht');
+//         }   
+//     catch (e) {
+//         console.log(e)
+//     }
+// });
