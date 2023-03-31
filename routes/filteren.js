14bf3e98a76708b3044bc435b2e947f834103067
JaidenHuijts <jaiden02huijts@gmail.com>
2023-03-31 13:56:13 +0200

    Filter post en begin error 404
    FIlter post doet het, alleen de data op de match pagina moet nog geladen worden. Ik heb een begin gemaakt aan de error 404 en die is vandaag af.

--- routes/filteren.js
+++ routes/filteren.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const router = express.Router()
-const { Match } = require('./matchSchema')
+const matchModel = require('../db/models/matchSchema');
+
+
 router.get('/', (req, res) => {
     res.render('./filteren')
-})
+});
 
 
+router.post('./matchen', async (req, res) => {
+  // console.log('@@-- test', req.params.id)
+  // fetch de sportschool by id
+  
+
+  // logic db call -> template aanroepen -> de data meegeven aan de template ||
+  console.log(req.body.geslacht) //In een req.body staat formulier data die je gepost heb
+  const match = await matchModel.find({
+    gender: req.body.geslacht,
+    leeftijd: req.body.leeftijd,
+    favgenres: req.body.muziekgenre
+  }).toArray();
+  console.log('@@-- data', match);
 
 
+  res.render('/.filteren', {
+    data: match
 
+
+  });
+});
 
   module.exports =  router
