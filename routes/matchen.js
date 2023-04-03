ac1d7f555b0a65b7e2457cdc8e4201aee1c56deb
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-04-03 21:05:20 +0200

    add matches to profile
    styling done for profile.ejs and matchen.ejs

--- routes/matchen.js
+++ routes/matchen.js
@@ -1,44 +1,53 @@
 const express = require('express')
 const router = express.Router()
 const matchModel = require('../db/models/matchSchema');
+const profileModel = require('../db/models/profileSchema');
+const authId = 99;
 
 
 router.get('/', async (req, res) => {
 
-    try{
-        const getUsers = await matchModel.find();
-        console.log(getUsers)
+    try {
+        const getMatches = await matchModel.find();
+        const profile = await profileModel.findOne({id: authId})
+        // Voeg twee arrays samen
+        const seenMatches = profile.matches.concat(profile.dislikes);
+         // Check of de user id's niet de id's van de matches en dislikes bevatten (seenMatches)
+        const potentialMatches = getMatches.filter(user => !seenMatches?.includes(user.id))
+    
+        res.render('matchen', {potentialMatches, profile})
 
-        res.render('./matchen', {getUsers})
-    } catch (error) {
+      } catch (error) {
         console.error(error)
-    }
-
+      }
 });
 
-// router.post('/match', async (req, res) => {
-//     const matchButtonLike = req.body.matchButtonLike;
-//     const matchButtonDislike = req.body.matchButtonDislike;
+// Met een button kan de gebruiker matches toevoegen aan zijn profiel of verwijderen.
+// 
+
+router.post('/match', async (req, res) => {
+    const matchButtonLike = req.body.matchButtonLike;
+    const matchButtonDislike = req.body.matchButtonDislike;
        
-//       if(matchButtonLike){
-//         try {
-//           await matchModel.findOneAndUpdate({id: 7}, {$addToSet:{matches: Number(matchButtonLike)}}, {
-//             new: true
+      if(matchButtonLike){
+        try {
+          await profileModel.findOneAndUpdate({id: authId}, {$addToSet:{matches: Number(matchButtonLike)}}, {
+            new: true
   
-//           })
-//         } catch(error) {
-//             console.error(error)
-//         }
-//       } else {
-//         try {
-//           await matchModel.findOneAndUpdate({id: 7}, {$addToSet:{dislikes: Number(matchButtonDislike)}}, {
-//             new: true
-//             })
-//         } catch(error) {
-//             console.error(error)
-//         }
-//       }
-//       res.redirect('/matches')
-// });
+          })
+        } catch(error) {
+            console.error(error)
+        }
+      } else {
+        try {
+          await profileModel.findOneAndUpdate({id: authId}, {$addToSet:{dislikes: Number(matchButtonDislike)}}, {
+            new: true
+            })
+        } catch(error) {
+            console.error(error)
+        }
+      }
+      res.redirect('/matchen')
+});
 
 module.exports =  router
