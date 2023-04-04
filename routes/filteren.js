b88e0805059a82221f893fb03bef73b8c8b89b47
JaidenHuijts <jaiden02huijts@gmail.com>
2023-04-04 15:44:46 +0200

    Kleine aanpassing code

--- routes/filteren.js
+++ routes/filteren.js
@@ -2,32 +2,22 @@ const express = require('express')
 const router = express.Router()
 const matchModel = require('../db/models/matchSchema');
 
-
 router.get('/', (req, res) => {
     res.render('./filteren')
 });
 
-
-router.post('./matchen', async (req, res) => {
-  // console.log('@@-- test', req.params.id)
+router.post('/matchen', async (req, res) => {
   // fetch de sportschool by id
   
-
   // logic db call -> template aanroepen -> de data meegeven aan de template ||
-  console.log(req.body.geslacht) //In een req.body staat formulier data die je gepost heb
+  console.log(req.body.geslacht, req.body.leeftijd, req.body.muziekgenre);//In een req.body staat formulier data die je gepost heb
   const match = await matchModel.find({
     gender: req.body.geslacht,
-    leeftijd: req.body.leeftijd,
+    age: req.body.leeftijd,
     favgenres: req.body.muziekgenre
-  }).toArray();
-  console.log('@@-- data', match);
-
-
-  res.render('./matchen', {
-    data: match
-
-
   });
+
+  res.redirect('/matchen');
 });
 
-  module.exports =  router
+module.exports =  router 
