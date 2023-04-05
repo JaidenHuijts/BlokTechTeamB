ec4de84086c0397e3ed07c3b26cf6310b2c4a005
JaidenHuijts <jaiden02huijts@gmail.com>
2023-04-05 13:04:36 +0200

    Filter werkend gemaakt
    Filter bug opgelost

--- routes/filteren.js
+++ routes/filteren.js
@@ -1,23 +1,62 @@
 const express = require('express')
 const router = express.Router()
 const matchModel = require('../db/models/matchSchema');
+const profileModel = require('../db/models/profileSchema');
+
 
 router.get('/', (req, res) => {
     res.render('./filteren')
 });
 
+// router.get('/newUser', async (req, res) => {
+//   try {
+//     const user = await profileModel.create({
+//     //_id: new ObjectId(11),
+//     userID: 11,
+//     image: "https://images.unsplash.com/photo-1520699918507-3c3e05c46b0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80",
+//     name: "Bas",
+//     gender: "Man",
+//     location: "Den Helder",
+//     age: "21",
+//     favgenres: "Pop",
+//     favtrack: "idk",
+//     favfestivals: ["a"],
+//     bio: "ja",
+//     matches: [],
+//     dislikes: []
+//   })
+
+//   const save = await user.save();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })
+
+
 router.post('/matchen', async (req, res) => {
-  // fetch de sportschool by id
-  
-  // logic db call -> template aanroepen -> de data meegeven aan de template ||
-  console.log(req.body.geslacht, req.body.leeftijd, req.body.muziekgenre);//In een req.body staat formulier data die je gepost heb
-  const match = await matchModel.find({
+  try {
+    // let ageGroup;
+    // const ageInt = Number(req.body.leeftijd);
+    // if (ageInt >= 18 && ageInt <= 25) {
+    //   ageGroup = "18";
+    // }
+
+    const potentialMatches = await matchModel.find({
     gender: req.body.geslacht,
     age: req.body.leeftijd,
-    favgenres: req.body.muziekgenre
+    favgenres: req.body.muziekgenre,
   });
 
-  res.redirect('/matchen');
+  console.log(potentialMatches);
+
+  res.render('./matchen', {potentialMatches});
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-module.exports =  router 
+  // res.redirect('matchen', {match});
+
+
+
+  module.exports =  router
